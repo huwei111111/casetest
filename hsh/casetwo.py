2424ac76c671b390173736753166e54e0a39659f
qudameng <qudameng@shinemo.com>
2023-03-29 16:19:51 +0800

    Changes

---
+++ hsh/casetwo.py
@@ -0,0 +1,29 @@
+import hashlib
+import hmac
+import time
+
+time = str(int(time.time() * 100000))
+s = ''
+
+
+class Sign:
+    def __init__(self, data):
+        self.data = data
+
+    def md(self):
+        global s
+        s = str(time + self.data)
+        s_md5 = hashlib.md5(s.encode()).hexdigest()
+        return s_md5
+
+    def sha2(self):
+        s_sha256 = hashlib.sha256(s.encode()).hexdigest()
+        return s_sha256
+
+    def sign(self):
+        s_hmac = hmac.new(str(s).encode(), digestmod=hashlib.sha256).hexdigest()
+        return s_hmac
+
+
+j = Sign('121212').sign()
+print(j)
